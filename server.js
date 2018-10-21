a6aac88f89518891cdf150992abda193c96790a1
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-10-21 21:43:44 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ async function getEvents() {
     for (var i = 0; i < events.events.length; i++) {
         var date = new Date(events.events[i].instanceList[0].startTime);
         if (!events.events[i].id.includes('shipevent_') && !events.events[i].id.includes('restrictedmodbattle_') && !events.events[i].id.includes('challenge_') )
-            message = message + "Event: " + events.events[i].nameKey.replace(/\[\/?[^\]]*\]/g, '').replace("\\n", " ") + " Start: " + date.format("dd.MM.yyyy HH:mm") + "UTC\n\r\n\r";
+            message = message + "Event: " + events.events[i].nameKey.replace(/\[\/?[^\]]*\]/g, '').replace("\\n", " ") + " Start: " + date.format("dd.mm.yyyy HH:MM") + "UTC\n\r\n\r";
     }
     
     return message;
@@ -122,7 +122,7 @@ async function getRaub() {
     for (var i = 0; i < events.events.length; i++) {
         var date = new Date(events.events[i].instanceList[0].startTime);
         if (events.events[i].id == 'EVENT_TRAINING_DROID_SMUGGLING' || events.events[i].id == 'EVENT_CREDIT_HEIST_GETAWAY_V2' || events.events[i].id == 'EVENT_RESOURCE_SMUGGLERS_RUN' || events.events[i].id == 'EVENT_RESOURCE_CONTRABAND_CARGO')
-            message = message + "Event: " + events.events[i].nameKey.replace(/\[\/?[^\]]*\]/g, '').replace("\\n", " ") + " Start: " + date.format("dd.MM.yyyy HH:mm") + "UTC\n\r\n\r";
+            message = message + "Event: " + events.events[i].nameKey.replace(/\[\/?[^\]]*\]/g, '').replace("\\n", " ") + " Start: " + date.format("dd.mm.yyyy HH:MM") + "UTC\n\r\n\r";
     }
 
     return message;
