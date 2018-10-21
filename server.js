5e3dac05f8fa56bafb50858152a308c4528d325a
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-10-21 21:13:42 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ async function getEvents() {
     console.log(events.events.length);
     for (var i = 0; i < events.events.length; i++) {
         if (!events.events[i].id.includes('shipevent_') && !events.events[i].id.includes('restrictedmodbattle_') && !events.events[i].id.includes('challenge_') )
-            message = message + "Event: " + events.events[i].nameKey.replace(/\[\/?[^\]]*\]/g, '').replace("\\n", " ") + " Start: " + new Date(events.events[i].instanceList[0].startTime + "\n\r\n\r");
+            message = message + "Event: " + events.events[i].nameKey.replace(/\[\/?[^\]]*\]/g, '').replace("\\n", " ") + " Start: " + new Date(events.events[i].instanceList[0].startTime).toString("dd.MM.yyy HH:mm") + "UTC\n\r\n\r";
     }
 
     console.log("out of loop");
