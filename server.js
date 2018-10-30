5a21aed64c76ffbc47b9993dc936781c37d37a85
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-10-30 08:48:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -143,8 +143,52 @@ async function handleEvent(event) {
 				
 		/** Get the zeta recommendations from swapi cacher */
 		let recommendations = await swapi.fetchZetas();
-				
-                message = words[1] + " recommendations is: " + recommendations.length;
+		
+		        let lim = 10;
+		let embed = {};
+		embed.title = `${player.name} - Next ${lim} best Zetas`;
+		embed.description = '`------------------------------`\n';
+			
+	    let availableZetas = [];
+        for( let z of recommendations.zetas ) {
+            let skill = player.roster.map(u => {
+                let ss = u.skills.filter(s => s.name === z.name);
+                if( ss.length === 0 ) { return null; }
+                
+                ss[0].rarity = u.rarity;
+                ss[0].level = u.level;
+                ss[0].gear = u.gear;
+                
+                return ss.length > 0 ? ss[0] : null;
+            });
+            skill = skill.filter(s => s);
+            
+            if( !skill || !skill[0] || skill[0].tier === 8 ) { continue; }
+            
+            z.rarity = skill[0].rarity;
+            z.gear = skill[0].gear;
+            availableZetas.push(z);
+        }
+        
+        availableZetas.sort((a,b) => {
+            return scoreZeta(a, player.roster) - scoreZeta(b, player.roster);
+        });
+        
+        
+        for( let az of availableZetas ) {
+            if( lim === 0 ) { break; }
+            
+            embed.description += '**'+az.toon+'** : '+az.name+'\n';
+            
+            --lim;
+        }
+        
+		embed.description += '`------------------------------`\n';
+
+		embed.color = 0x936EBB;
+		embed.timestamp = today;
+	
+                message = embed;
 				
 				
 			}catch(err){
@@ -259,3 +303,16 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`listening on ${port}`);
 });
+
+function scoreZeta( zeta, roster ) {
+    let rankedScore = zeta.pvp * zeta.tw * zeta.tb * zeta.pit * zeta.tank * zeta.sith;
+    if( rankedScore === 0 ) { rankedScore = 999 }
+    
+    let rosterScore = scoreRoster( zeta, roster );
+    return rankedScore - rosterScore;
+}
+
+function scoreRoster( zeta, roster ) {
+    // To-do : build a roster score based on squad support for zeta
+    return (zeta.gear * zeta.rarity);
+}
