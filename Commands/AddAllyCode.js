b9f046856e0a9cfd1118f84e833f6e8eea914c21
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-11 16:39:55 +0100

    Update AddAllyCode.js

--- Commands/AddAllyCode.js
+++ Commands/AddAllyCode.js
@@ -1,5 +1,5 @@
 const sql = require('mssql');
-const pushmessage = require('../Pushmessage');
+const pushmessage = require('../Commands/Pushmessage');
 
 module.exports = async ( lineid ) => {
 	try {
