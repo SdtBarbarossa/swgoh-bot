cc86a0af8029065398762c44fe93fb4b5ec714a1
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-11 19:36:32 +0100

    Update AddAllyCode.js

--- Commands/AddAllyCode.js
+++ Commands/AddAllyCode.js
@@ -8,10 +8,11 @@ module.exports = async ( lineidNow, allycode ) => {
         const result = await sql.query`select * from lineidToAllycode where lineId = ${lineidNow}`;
 		
 	
-	if(result.recordset.length == 0)
+	if(result.recordset.length == 0 || result.recordset[0].allycode == null)
 	{
 		console.log(result);
-		const resultAdd = await sql.query`insert into lineidToAllycode(lineId, allycode) Values(${lineidNow},${Number(allycode)})`;
+		let allycodeAsNumber = Number(allycode);
+		const resultAdd = await sql.query`insert into lineidToAllycode(lineId, allycode) Values(${lineidNow},${allycodeAsNumber})`;
 		
 	pushmessage(lineidNow, "added you to with allycode " + allycode);
 	}
