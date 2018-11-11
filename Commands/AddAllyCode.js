5bbb1026a91c015284877055baff21793b277315
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-11 16:00:28 +0100

    Create AddAllyCode.js

---
+++ Commands/AddAllyCode.js
@@ -0,0 +1,14 @@
+const sql = require('mssql');
+
+module.exports = async ( lineid ) => {
+	try {
+		await sql.connect('mssql://linebotdb:Wk99lNRnQ~_y@den1.mssql7.gear.host/linebotdb')
+        const result = await sql.query`select * from loineidToAllycode where lindeId = ${lineid}`
+        console.log(result);
+        return result;
+	} catch(e) {
+  console.log(e.message);
+  return e.message;
+	}
+
+}
