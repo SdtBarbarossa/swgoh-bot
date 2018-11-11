274390f8bbfa8fa728b7d6c569a18c031033596e
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-11 17:07:20 +0100

    Update AddAllyCode.js

--- Commands/AddAllyCode.js
+++ Commands/AddAllyCode.js
@@ -4,8 +4,6 @@ const pushmessage = require('../Commands/Pushmessage');
 module.exports = async ( lineidNow ) => {
 	try {
 	await sql.connect('mssql://linebotdb:Wk99lNRnQ~_y@den1.mssql7.gear.host/linebotdb');
-        let result = await db.request()
-            .query('select * from lineidToAllycode where lineId = ' + lineidNow);
         const result = await sql.query`select * from lineidToAllycode where lineId = ${lineidNow}`
 	
 	if(result == "")
