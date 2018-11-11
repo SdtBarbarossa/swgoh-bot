5c6715528ae92213697ef159e7c5f19ce01760e5
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-11 17:10:13 +0100

    Update AddAllyCode.js

--- Commands/AddAllyCode.js
+++ Commands/AddAllyCode.js
@@ -8,15 +8,15 @@ module.exports = async ( lineidNow ) => {
 	
 	if(result == "")
 	{
-	pushmessage(lineid, "didnt found you in db");
+	pushmessage(lineidNow, "didnt found you in db");
 	}
 	
         console.log(result);
-	pushmessage(lineid, result);
+	pushmessage(lineidNow, result);
 		
 	} catch(e) {
   console.log(e.message);
-	pushmessage(lineid, e.message);
+	pushmessage(lineidNow, e.message);
 	}
 
 }
