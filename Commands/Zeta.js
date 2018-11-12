a5ddc34bd131508f69a09c452aa7df0ed64c819c
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-12 07:45:57 +0100

    Update Zeta.js

--- Commands/Zeta.js
+++ Commands/Zeta.js
@@ -1,14 +1,116 @@
 const pushmessage = require('../Commands/Pushmessage');
 const getAllyCode = require('../Commands/GetAllyCode');
 
-module.exports = async ( lineidNow, groupId ) => {
+const ApiSwgohHelp = require('api-swgoh-help');
+const swapi = new ApiSwgohHelp({
+    "username": process.env.API_USERNAME,
+    "password": process.env.API_PASSWORD
+});
+
+module.exports = async ( lineidNow, groupId, criteriaNow ) => {
 	try {
   let allyCodeNow = await GetAllyCode( lineidNow, groupId );
-  pushmessage(groupId, "Your allycode is : " + allyCodeNow);
+		
+var payload = {
+		"allycode" : allyCodeNow,
+        	"language": "ENG_US"
+    		};
+let player = (await swapi.fetchPlayer(payload))[0];
+		let answer = await getZeta(player, criteriaNow);
+  pushmessage(groupId, answer);
 	} catch(e) {
   		console.log(e.message);
 		sql.close();
 		pushmessage(groupId, e.message);
 	}
 
+}
+
+async function getZeta(player, criteria){
+	
+	var message = "";
+		
+	try{
+		var payload = {
+        	"language": "ENG_US"
+    		};
+		
+		/** Get the zeta recommendations from swapi cacher */
+		let recommendations = await swapi.fetchAPI( swapi.zetas, payload );
+		
+		let today = new Date();
+		
+		let lim = 10;
+		message += `${player.name} - Next ${lim} best Zetas`;
+		message += criteria ? ' Filtered by : '+criteria+' \n' : '';
+		message += '\n------------------------------\n';
+		
+		
+	    let availableZetas = [];
+		
+        for( let z of recommendations.zetas ) {
+				
+            let skill = player.roster.map(u => {
+				    
+                let ss = u.skills.filter(s => s.nameKey === z.name);
+                if( ss.length === 0 ) { return null; }
+		    		    
+                ss[0].rarity = u.rarity;
+                ss[0].level = u.level;
+                ss[0].gear = u.gear;
+                
+                return ss.length > 0 ? ss[0] : null;
+		    
+            });
+	    		
+            skill = skill.filter(s => s);
+            
+            if( !skill || !skill[0] || skill[0].tier === 8 ) { continue; }
+            
+            z.rarity = skill[0].rarity;
+            z.gear = skill[0].gear;
+            availableZetas.push(z);
+        }
+        		
+        if( criteria ) {
+            availableZetas.sort((a,b) => {
+                return scoreZeta(a[criteria], player.roster) - scoreZeta(b[criteria], player.roster) >= 0 ? 1 : -1;
+            });
+        } else {
+            availableZetas.sort((a,b) => {
+                return scoreZeta(a, player.roster) - scoreZeta(b, player.roster) >= 0 ? 1 : -1;
+            });
+        }  
+                
+        for( let az of availableZetas ) {
+            if( lim === 0 ) { break; }
+            
+            message += ''+az.toon+' : '+az.name+'\n';
+            
+            --lim;
+        }
+        
+		message += '------------------------------\n';
+		message += 'Optional filter criteria :\n pvp, tw, tb, pit, tank, sith, versa\n';
+				
+			}catch(err){
+			message = err.message;	
+			}
+
+	return message;
+}
+
+function scoreZeta( zeta, roster ) {
+    if( typeof zeta === 'number' ) return parseInt(zeta);
+
+    let rankedScore = zeta.pvp * zeta.tw * zeta.tb * zeta.pit * zeta.tank * zeta.sith;
+    if( rankedScore === 0 ) { rankedScore = 999 }
+    
+    let rosterScore = scoreRoster( zeta, roster );
+    return rankedScore - rosterScore;
+}
+
+function scoreRoster( zeta, roster ) {
+    // To-do : build a roster score based on squad support for zeta
+    return (zeta.gear * zeta.rarity);
 }
