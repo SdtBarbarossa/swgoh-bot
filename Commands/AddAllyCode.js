ffe03e228e3659bd0d7f4c28202eee3c5a6e8d66
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-12 07:04:23 +0100

    Update AddAllyCode.js

--- Commands/AddAllyCode.js
+++ Commands/AddAllyCode.js
@@ -1,7 +1,7 @@
 const sql = require('mssql');
 const pushmessage = require('../Commands/Pushmessage');
 
-module.exports = async ( lineidNow, allycode ) => {
+module.exports = async ( lineidNow, allycode, groupId ) => {
 	try {
 	await sql.connect('mssql://linebotdb:Wk99lNRnQ~_y@den1.mssql7.gear.host/linebotdb');
 	
@@ -10,36 +10,18 @@ module.exports = async ( lineidNow, allycode ) => {
 	let allycodeAsNumber = Number(allycode);
 	if(result.recordset.length == 0 )
 	{
-		console.log(result);
-		
 		const resultAdd = await sql.query`insert into lineidToAllycode(lineId, allycode) Values(${lineidNow},${allycodeAsNumber})`;
-		
-	pushmessage(lineidNow, "added you to with allycode " + allycode);
+		pushmessage(groupId, "added you to with allycode " + allycode);
 	}
-		else{
-			
-			if(result.recordset[0].allycode == null){
-			const resultUpdate = await sql.query`update lineidToAllycode set allycode = ${allycodeAsNumber} where lineId = ${lineidNow}`;
-		pushmessage(lineidNow, "Updated your allycode to " + allycode);
-			}
-			else{
-			
-
-		console.log(result.recordset);
-	pushmessage(lineidNow, "I already added you with " + result.recordset[0].allycode);
-			}
-		}
-		
-		
-		
+	else{
+		const resultUpdate = await sql.query`update lineidToAllycode set allycode = ${allycodeAsNumber} where lineId = ${lineidNow}`;
+		pushmessage(groupId, "Updated your allycode to " + allycode);
+	}
 	sql.close();
-
-		
 	} catch(e) {
-  console.log(e.message);
+  		console.log(e.message);
 		sql.close();
-		
-	pushmessage(lineidNow, e.message);
+		pushmessage(groupId, e.message);
 	}
 
 }
