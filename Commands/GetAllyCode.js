a2c02bade395d341ef435bd5e202c5bb5c9de3f6
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-12 07:29:40 +0100

    Update and rename Zeta.js to GetAllyCode.js

---
+++ Commands/GetAllyCode.js
@@ -0,0 +1,15 @@
+const pushmessage = require('../Commands/Pushmessage');
+
+module.exports = async ( lineidNow, groupId ) => {
+	try {
+	await sql.connect('mssql://linebotdb:Wk99lNRnQ~_y@den1.mssql7.gear.host/linebotdb');
+        const result = await sql.query`select * from lineidToAllycode where lineId = ${lineidNow}`;
+	sql.close();
+	return result.recordset[0].allycode;
+	} catch(e) {
+  		console.log(e.message);
+		sql.close();
+		pushmessage(groupId, e.message);
+	}
+
+}
