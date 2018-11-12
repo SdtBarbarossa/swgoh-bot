ffbc05789272ed9749152fb334ab4877a8b972fc
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-12 21:24:06 +0100

    Update GetAllyCode.js

--- Commands/GetAllyCode.js
+++ Commands/GetAllyCode.js
@@ -6,6 +6,7 @@ module.exports = async ( lineidNow, groupId ) => {
 	await sql.connect('mssql://linebotdb:Wk99lNRnQ~_y@den1.mssql7.gear.host/linebotdb');
         const result = await sql.query`select * from lineidToAllycode where lineId = ${lineidNow}`;
 	sql.close();
+	console.log('result.recordset', result.recordset);
 	return result.recordset[0].allycode;
 	} catch(e) {
   		console.log(e.message);
