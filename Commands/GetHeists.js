c99885fea8730fda948acce550dd55e03bc68ff0
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-12 20:40:08 +0100

    Create GetHeists.js

---
+++ Commands/GetHeists.js
@@ -0,0 +1,30 @@
+const pushmessage = require('../Commands/Pushmessage');
+
+const ApiSwgohHelp = require('api-swgoh-help');
+const swapi = new ApiSwgohHelp({
+    "username": process.env.API_USERNAME,
+    "password": process.env.API_PASSWORD
+});
+
+module.exports = async ( groupId ) => {
+	try {
+    let message = "";
+    
+    var payload = {
+        "language": "ENG_US"
+    };
+    let events = await swapi.fetchEvents(payload);
+    
+    for (var i = 0; i < events.events.length; i++) {
+        var date = new Date(events.events[i].instanceList[0].startTime);
+        if (events.events[i].id == 'EVENT_TRAINING_DROID_SMUGGLING' || events.events[i].id == 'EVENT_CREDIT_HEIST_GETAWAY_V2' || events.events[i].id == 'EVENT_RESOURCE_SMUGGLERS_RUN' || events.events[i].id == 'EVENT_RESOURCE_CONTRABAND_CARGO')
+            message = message + "Event: " + events.events[i].nameKey.replace(/\[\/?[^\]]*\]/g, '').replace("\\n", " ") + " Start: " + date.format("dd.mm.yyyy HH:MM") + "UTC\n\r\n\r";
+    }
+    
+		pushmessage(groupId, message);
+	} catch(e) {
+  		console.log(e.message);
+		pushmessage(groupId, e.message);
+	}
+
+}
