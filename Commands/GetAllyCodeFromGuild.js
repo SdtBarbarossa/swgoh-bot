c8b417583ec6cd5c959f576218d2352d0c7b2f88
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-12 21:03:57 +0100

    Create GetAllyCodeFromGuild.js

---
+++ Commands/GetAllyCodeFromGuild.js
@@ -0,0 +1,36 @@
+const pushmessage = require('../Commands/Pushmessage');
+const getAllyCode = require('../Commands/GetAllyCode');
+
+const ApiSwgohHelp = require('api-swgoh-help');
+const swapi = new ApiSwgohHelp({
+    "username": process.env.API_USERNAME,
+    "password": process.env.API_PASSWORD
+});
+
+module.exports = async ( groupId, membername ) => {
+	try {
+  let message = "";
+  let allyCodeNow = await getAllyCode( groupId, groupId );
+  
+  var payload = {
+	"allycode" : allyCodeNow,
+  "language": "ENG_US"
+   };
+   
+   let guildNew = await swapi.fetchGuild(payload);
+   let guild = guildNew[0];
+   
+   let memberNow = guild.roster.find(function(mem) {
+            return mem.name.toLowerCase() == membername.toLowerCase();
+        	}
+	);
+    
+  message += membername + " allycode is " + memberNow.allyCode;
+  
+	pushmessage(groupId, message);
+	} catch(e) {
+  		console.log(e.message);
+		pushmessage(groupId, e.message);
+	}
+
+}
