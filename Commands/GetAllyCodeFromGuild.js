e5c9c8f662bdcafd43deb97a2fc42f4ad6f11e7e
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-12 21:11:23 +0100

    Update GetAllyCodeFromGuild.js

--- Commands/GetAllyCodeFromGuild.js
+++ Commands/GetAllyCodeFromGuild.js
@@ -11,15 +11,20 @@ module.exports = async ( groupId, membername ) => {
 	try {
   let message = "";
   let allyCodeNow = await getAllyCode( groupId, groupId );
-  
+
+  console.log('allyCodeNow', allyCodeNow);
+		
   var payload = {
 	"allycode" : allyCodeNow,
   "language": "ENG_US"
    };
    
    let guildNew = await swapi.fetchGuild(payload);
-   let guild = guildNew[0];
    
+   console.log('guildNew', guildNew);
+   let guild = guildNew[0];
+   console.log('guild', guild);
+		
    let memberNow = guild.roster.find(function(mem) {
             return mem.name.toLowerCase() == membername.toLowerCase();
         	}
