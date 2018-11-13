793282988081b1133d6ebb353f9e21b3106a0ebf
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-13 11:07:04 +0100

    Update SquadRecommendation.js

--- Commands/SquadRecommendation.js
+++ Commands/SquadRecommendation.js
@@ -9,7 +9,7 @@ const swapi = new ApiSwgohHelp({
 
 module.exports = async ( groupId ) => {
 	try {
-		let answer = await getSquadRecommendations();
+		let answer = await getSquadRecommendations('sith', '1');
   pushmessage(groupId, answer);
 	} catch(e) {
   		console.log(e.message);
@@ -18,17 +18,29 @@ module.exports = async ( groupId ) => {
 
 }
 
-async function getSquadRecommendations(){
+async function getSquadRecommendations(criteria, phase){
 	
 	var message = "";
 		
 	try{
+		let phaseAsNumber = Number(phase)-1;
 		let recommendations = await swapi.fetchSquads({});
     
-		console.log('recommendations[sith]',recommendations['sith']);
-		console.log('recommendations[sith].phase[0]',recommendations['sith'].phase[0]);
+		console.log('recommendations[sith].phase[0]',recommendations[criteria].phase[phaseAsNumber]);
 		
-		message += `Recommendation keys: ${Object.keys(recommendations).join('\n')}\n`;
+		message += recommendations[criteria].phase[phaseAsNumber].name + "\n";
+		
+		let squadsForPhase = recommendations[criteria].phase[phaseAsNumber].squads;
+		
+		for(let s in squadsForPhase){
+			message += s.name + "\n";
+			message += s.note + "\n";
+			for(let t in s.team){
+				message += t + "\n";
+			    }
+		}
+		
+		message += `Recommendation keys: \n${Object.keys(recommendations).join('\n')}\n`;
 				
 			}catch(err){
 			message = err.message;	
