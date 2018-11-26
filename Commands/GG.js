fa5ee510c2970d50c17628a248c1813c39ee9288
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-11-26 20:32:44 +0100

    Create GG.js

---
+++ Commands/GG.js
@@ -0,0 +1,72 @@
+Episode III – Die Rache der Sith (Zitate)
+„Eine lächerliche Befreiungsaktion!“
+— General Grievous zu Anakin und Obi-Wan auf der Kreuzerbrücke (Episode III – Die Rache der Sith, Kapitel 6 – Konfrontation mit Grievous)
+Grievous: „Bei jemandem mit deinem Ruf hätte ich erwartet, dass du ein wenig älter wärst.“
+Anakin Skywalker: „General Grievous, Ihr seid kleiner als ich erwartet hätte.“
+Grievous: „Jedi-Abschaum!“
+Obi-Wan Kenobi: „Wir haben etwas zu erledigen, Anakin. Du solltest ihn nicht provozieren.“
+— Grievous und Anakin Skywalker nachdem Grievous sie gefangen nahm (Episode III – Die Rache der Sith, Kapitel 6 – Konfrontation mit Grievous)
+„Eure Laserschwerter werden meine Sammlung hervorragend ergänzen.“
+— General Grievous bei der Gefangennahme von Anakin Skywalker und Obi-Wan Kenobi (Episode III – Die Rache der Sith, Kapitel 6 – Konfrontation mit Grievous)
+„Zermalmt sie - sie sollen leiden!“
+— Grievous beim Kampf mit Obi-Wan Kenobi und Anakin Skywalker auf der Brücke der Unsichtbaren Hand (Episode III – Die Rache der Sith, Kapitel 6 – Konfrontation mit Grievous)
+„General Kenobi, Eure Kühnheit ist beeindruckend.“
+— Grievous zu Obi-Wan Kenobi, als dieser ihn auf Utapau gefunden hatte (Episode III – Die Rache der Sith, Kapitel 21 – Obi-Wan gegen Grievous)
+„Dieser Jedi-Abschaum gehört mir!“
+— General Grievous kurz vor dem Kampf mit Obi-Wan Kenobi auf Utapau in Pau City (Episode III – Die Rache der Sith, Kapitel 21 – Obi-Wan gegen Grievous)
+„Armee hin oder her, Euch muss klar sein, dass Ihr verloren seid!“
+— General Grievous im Kampf gegen Obi-Wan Kenobi (Episode III – Die Rache der Sith, Kapitel 21 – Obi-Wan gegen Grievous)
+Fernsehserien
+Clone Wars
+„Jedi! Ihr seid umzingelt. Eure Truppen sind dezimiert. Schließt Frieden mit der Macht, denn dies ist eure letzte Stunde. Doch ihr sollt wissen, dass ich General Grievous nicht vollkommen ohne Gnade bin. Ich werde euch den Ehrentod der Krieger gewähren - bereitet euch vor!“
+— Grievous zu sechs Jedi kurz bevor er sie angriff (Clone Wars Vol. I, Kapitel 20)
+„Lauf, Jedi, lauf. Ihr zögert das Unvermeidliche nur hinaus!“
+— Grievous nachdem Ki-Adi-Mundi geflohen ist (Clone Wars Vol. I, Kapitel 20)
+„Eure Ausbildung hat mir viel gebracht. Ich habe zahlreiche Trophäen errungen.“
+— Grievous zu Dooku (Clone Wars Vol. I, Kapitel 20)
+„Die Strategie funktioniert vorzüglich, mein Lord. Die Jedi und ihre Streitkräfte sind bis an die Rand-Gebiete der Galaxis verteilt, sie versuchen vergeblich, unseren nächsten Angriff zu vereiteln.“
+— Grievous zu Darth Sidious (Clone Wars Vol. II, Kapitel 22)
+„Und so endet es also. Ein kühnes Unterfangen, doch wie man sieht, letztendlich nutzlos. Und jetzt, Jedi, macht euch bereit, denn Ihr werdet nun diese Welt verlassen.“
+— Grievous zu Shaak Ti nachdem er zwei Jedi besiegt hat (Clone Wars Vol. II, Kapitel 24)
+„Ihr gehört mir, alter Mann.“
+— Grievous zu Palpatine (Clone Wars Vol. II, Kapitel 24)
+„Ihr seid müde, Jedi. Das werdet Ihr nicht mehr brauchen. Ich habe etwas anderes für euch.“
+— Grievous zu Shaak Ti nachdem er sie entwaffnete und fesselte (Clone Wars Vol. II, Kapitel 24)
+The Clone Wars
+Der Angriff der Malevolence
+„Ein weiterer erfolgreicher Test, findet ihr nicht auch, Count Dooku?“
+— Grievous zu Dooku nach dem Abschießen der Waffe der Malevolence (Der Angriff der Malevolence)
+„Jetzt wird die Republik alles über unsere Ionenkanone erfahren. Zurück an die Arbeit.“
+— Grievous zu Dooku nach dem Abfeuern der Ionenkanone des neuen Separatistenschiffes (Der Angriff der Malevolence)
+Die Zerstörung der Malevolence
+„Ich dulde keinerlei Versagen.“
+— Grievous schärft seinen Droiden die Wichtigkeit der Reparatur des Hyperantriebs ein. (Die Zerstörung der Malevolence)
+In den Fängen von Grievous (Zitate)
+„Ihr verlangt von mir den Sieg über die Jedi - Ihr gebt mir aber nichts als Kampfdroiden zu Verfügung?“
+— General Grievous, nachdem er von Dooku benachrichtigt wurde (In den Fängen von Grievous)
+„Ich töte euch alle! Hörst du mich, Jedi?“
+— General Grievous zu Kit Fisto nachdem er seinen Padawan Nahdar Vebb getötet hatte (In den Fängen von Grievous)
+Grievous Hinterhalt
+„Meister Koth, nicht wahr? Auf diese Begegnung habe ich mich schon lange gefreut.“
+— Grievous zu Eeth Koth (Grievous' Hinterhalt)
+„Mörder? Ist es Mord, wenn man die Galaxis von euch Jedi-Abschaum befreit?“
+— Grievous zu Eeth Koth (Grievous Hinterhalt)
+„Seid gegrüßt, Jedi. Wieder einmal ist ein Mitglied eures Ordens in meine Fänge geraten. Doch was noch besser ist: Es handelt sich um ein führendes Mitglied eures Jedi-Rates. Jetzt hört mir gut zu, Jedi: Ich schere mich nicht um eure Politik, ich schere mich nicht um eure Republik. Ich lebe einzig und allein, um euch sterben zu sehen.“
+— Grievous in seiner Botschaft zum Jedi-Tempel (Grievous' Hinterhalt)
+„Das ist das Ende des Obi-Wan Kenobi!“
+— Grievous zu Obi-Wan Kenobi in ihrem Duell (Grievous Hinterhalt)
+Spiele
+Episode III - Die Rache der Sith (Spiel)
+„Kämpft weiter, doch es wird euch nichts nützen!“
+— Grievous zu Anakin und Obi-Wan als sie gegen seine Magnawächter kämpfen. (Episode III - Die Rache der Sith (Spiel), Storymission 6 Die rechte Hand des Generals)
+„Ihr seht eurem Ende entgegen, Kenobi!“
+— Grievous zu Obi-Wan (Episode III - Die Rache der Sith (Spiel), Storymission 9 Showdown mit Grievous)
+„Ihr seid veraltet!“
+— Grievous zu Klonkriegern, kurz bevor er sie tötete (Episode III - Die Rache der Sith (Spiel), Bonusmission 2 Grievous auf der Flucht)
+„Die Macht kann euch nicht schützen!“
+— Grievous zu Obi-Wan (Episode III - Die Rache der Sith (Spiel), Bonusmission 2 Grievous auf der Flucht)
+„Ihr seid gut, Meister Kenobi, aber nicht gut genug!“
+— Grievous zu Obi-Wan (Episode III - Die Rache der Sith (Spiel), Bonusmission 2 Grievous auf der Flucht)
+The Clone Wars (Handyspiel)
+„Ziemlich leer, was? Keine Angst, Klonschleim, bald hast du jede Menge Gesellschaft. Du und deine lächerliche Republik seid bald am Ende!“
+— Grievous' Nachricht an Denals Truppe. (The Clone Wars Handyspiel, Level 13 Geheimbasis)
