2440afa5ef6ba0c664224b6cb6cc8c46e4312609
SdtBarbarossa <DaMicha_essen@gmx.de>
2018-12-25 14:11:01 +0100

    Update Pushmessage.js

--- Commands/Pushmessage.js
+++ Commands/Pushmessage.js
@@ -16,7 +16,16 @@ let pmessage = {
   		type: 'text',
   		text: message
 		};
-
+		
+if(message.length > 1900){
+	console.log("The Message is very long. Look at this: " + message.length);
+	var splitMessages = splitNChars(message, 1900);
+	let pmessage = {
+  		type: 'text',
+  		text: splitMessages
+		};
+}
+	
 client.pushMessage(destinationId, pmessage)
   .then(() => {
     console.log('pushed');
@@ -28,4 +37,12 @@ client.pushMessage(destinationId, pmessage)
 	} catch(e) {
   console.log(e.message);
 	}
+}
+
+function splitNChars(txt, num) {
+  var result = [];
+  for (var i = 0; i < txt.length; i += num) {
+    result.push(txt.substr(i, num));
+  }
+  return result;
 }
