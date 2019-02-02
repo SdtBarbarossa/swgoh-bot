b9846414dfa0212f07bf3ccdf158bb78fd3c2acf
SdtBarbarossa <DaMicha_essen@gmx.de>
2019-02-02 09:05:18 +0100

    Update Spunkte.js

--- Commands/Spunkte.js
+++ Commands/Spunkte.js
@@ -1,1 +1,10 @@
+const pushmessage = require('../Commands/Pushmessage');
 
+module.exports = async ( groupId ) => {
+	try {
+    pushmessage(groupId, "test");
+	} catch(e) {
+  		console.log(e.message);
+	}
+
+}
