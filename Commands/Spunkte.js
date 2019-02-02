ded815194e7462e3f47f593bd5f5c20777793671
SdtBarbarossa <DaMicha_essen@gmx.de>
2019-02-02 09:06:37 +0100

    Update Spunkte.js

--- Commands/Spunkte.js
+++ Commands/Spunkte.js
@@ -1,8 +1,24 @@
 const pushmessage = require('../Commands/Pushmessage');
+const getAllyCode = require('../Commands/GetAllyCode');
 
-module.exports = async ( groupId ) => {
+const ApiSwgohHelp = require('api-swgoh-help');
+const swapi = new ApiSwgohHelp({
+    "username": process.env.API_USERNAME,
+    "password": process.env.API_PASSWORD
+});
+
+module.exports = async ( lineidNow, groupId, criteriaNow ) => {
 	try {
-    pushmessage(groupId, "test");
+  let allyCodeNow = await getAllyCode( lineidNow, groupId );
+		
+var payload = {
+		"allycode" : allyCodeNow,
+        	"language": "ENG_US"
+    		};
+let player = (await swapi.fetchPlayer(payload));
+		player = player.result[0];
+		
+    pushmessage(groupId, "Player: " + player.name);
 	} catch(e) {
   		console.log(e.message);
 	}
