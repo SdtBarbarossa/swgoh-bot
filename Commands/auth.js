c3dcd1ee5e66b41273121491e6405d50276c3604
SdtBarbarossa <DaMicha_essen@gmx.de>
2019-02-02 09:38:37 +0100

    Update auth.js

--- Commands/auth.js
+++ Commands/auth.js
@@ -1,11 +1,11 @@
-const {GoogleAuth} = require('google-auth-library');
+const GoogleAuth = require('google-auth-library');
 
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 
 function authorize() {
     return new Promise(resolve => {
-        const authFactory = new GoogleAuth();
-        const jwtClient = new authFactory.JWT(
+        const authFactory = new GoogleAuth.GoogleAuth();
+        const jwtClient = new GoogleAuth.JWT(
             process.env.GOOGLE_CLIENT_EMAIL,
             null,
             process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), 
