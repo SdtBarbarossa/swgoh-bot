2b45db2318ffab3b079c1f010f769f0b617e2976
SdtBarbarossa <DaMicha_essen@gmx.de>
2019-02-02 09:18:03 +0100

    Update Spunkte.js

--- Commands/Spunkte.js
+++ Commands/Spunkte.js
@@ -7,6 +7,12 @@ const swapi = new ApiSwgohHelp({
     "password": process.env.API_PASSWORD
 });
 
+const google = require('googleapis');
+const sheetsApi = google.sheets('v4');
+const googleAuth = require('../Commands/auth');
+
+const SPREADSHEET_ID = '1b3zv_jMmec8AjHFHulWLz3iOvc-UW_EYLFchZwfJFzI';
+
 module.exports = async ( lineidNow, groupId, criteriaNow ) => {
 	try {
   let allyCodeNow = await getAllyCode( lineidNow, groupId );
@@ -18,7 +24,28 @@ var payload = {
 let player = (await swapi.fetchPlayer(payload));
 		player = player.result[0];
 		
-    pushmessage(groupId, "Player: " + player.name);
+		googleAuth.authorize()
+    .then((auth) => {
+        sheetsApi.spreadsheets.values.get({
+            auth: auth,
+            spreadsheetId: SPREADSHEET_ID,
+            range: "'Sheet1'!A1:Q60",
+        }, function (err, response) {
+            if (err) {
+                console.log('The API returned an error: ' + err);
+        	pushmessage(groupId, 'The API returned an error:' + err);
+                return console.log(err);
+            }
+            var rows = response.values;
+            console.log(null, rows);
+    	    pushmessage(groupId, "rows: " + rows);
+        });
+    })
+    .catch((err) => {
+    pushmessage(groupId, "auth error: " + err);
+        console.log('auth error', err);
+    });
+		
 	} catch(e) {
   		console.log(e.message);
 	}
